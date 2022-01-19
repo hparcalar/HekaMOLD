833adb7f149a4c0ccbca91c3dfdedff3c6af39ca
hakanstlms <91190415+hakanstlms@users.noreply.github.com>
2022-01-19 16:46:04 +0300

    LsabitAdded

---
+++ Heka.DataAccess/Context/LSabit.cs
@@ -0,0 +1,9 @@
+﻿namespace Heka.DataAccess.Context
+{
+    public static class LSabit
+    {
+        public static string GET_ITHALAT { get { return "İtlahat"; } }
+        public static string GET_IHRACAT { get { return "İhracat"; } }
+
+    }
+}
